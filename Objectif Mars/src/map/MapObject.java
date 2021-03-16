4fcc87c8ba1f2f983c65fa6df21caf96e5c1f449
Clément Maurin <clementmaurin.off@gmail.com>
2021-03-16 10:12:39 +0100

    First robot working, added comments on previous classes

--- Objectif Mars/src/map/MapObject.java
+++ Objectif Mars/src/map/MapObject.java
@@ -1,9 +1,13 @@
 package map;
 
 public class MapObject {
+    /** Position of the object within a line */
     private int posX;
+    /** Position of the object within a collumn */
     private int posY;
+    /** Name that describe the object (it's used later on to display each element */
     private String name;
+    /** Char representation of the object that can be stored in a text file */
     public char mapRepresentation;
 
     /**
@@ -18,6 +22,45 @@ public class MapObject {
         this.posY = posY;
         this.name = name;
         this.mapRepresentation = mapRepresentation;
+    }
+
+    /**
+     * Create an object without specifying the name. It'll be given automatically based on it's char representation
+     * @param posX the position of the object on the X axis (within a line)
+     * @param posY the position of the object on the Y axis (within a collumn)
+     * @param mapRepresentation The character representation of the object on the map
+     */
+    public MapObject(int posX, int posY, char mapRepresentation) {
+        this.posX = posX;
+        this.posY = posY;
+        this.mapRepresentation = mapRepresentation;
+        this.name = getName(mapRepresentation);
+    }
+
+    /**
+     * @return the name of the object (ex: "sable")
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Return the name of the element described as it's char representation
+     * @param representation the char representation of an element on the map
+     * @return the name of the given element
+     */
+    public String getName(char representation) {
+        switch (representation) {
+            case '*': return "Diamant";
+            case '%': return "Rocher dur";
+            case '.': return "Sable";
+            case ':': return "Gres";
+            case '=': return "Roche volcanique";
+            case '-': return "Petit rocher";
+            case '#': return "Rubis";
+            case '@': return "Base";
+        }
+        return "NC";
     }
 
     /**
--- Objectif Mars/src/sample/Main.java
+++ Objectif Mars/src/sample/Main.java
@@ -4,11 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import map.Map;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Main extends Application {
 
@@ -18,18 +21,32 @@ public class Main extends Application {
         primaryStage.setTitle("Objectif Mars");
         GridPane pane = new GridPane();
 
+        /* Create a Map with the default map representation file */
         Map map = new Map();
         char[][] array = map.getRepresentation();
         int sizeX = map.getSizeX();
         int sizeY = map.getSizeY();
 
+        /* For each char in the map char representation : */
         for (int x = 0; x < sizeX; x++) {
             for (int y = 0; y < sizeY; y++) {
-                Label label = new Label("" + array[y][x], new Rectangle(600 / sizeY, 900 / sizeX));
-                pane.add(label, x, y);
+                try {
+                    /* Create an image with the given texture for each position of each representations */
+                    Image image = new Image(new FileInputStream("textures/" + map.getObject(x, y).getName().toLowerCase() + ".png"));
+                    ImageView iv = new ImageView(image);
+                    /* Set the size of each parcel to it feet a good looking view. */
+                    iv.setFitHeight(25);
+                    iv.setFitWidth(25);
+                    /* Add each images on the given x:y position in the pane */
+                    pane.add(iv, x, y);
+                } catch (FileNotFoundException e) {
+                    System.err.println("WARNING: You've given a bad texture file/name. A default texture as" +
+                            "been displayed but please change it as soon as possible to avoid further display issues.");
+                }
             }
         }
 
+        /* Add the GridPane into the scene panel */
         Scene scene = new Scene(pane);
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.setScene(scene);
